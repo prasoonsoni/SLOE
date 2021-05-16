62c2e231a409cdedc89f2674fa548ba927c846d3
Arjun <arjun140702@gmail.com>
2021-05-16 15:56:13 +0530

    Splash Screen UI changes

--- app/src/main/java/com/prasoonsoni/supervisedlearninginonlineeducation/SplashActivity.java
+++ app/src/main/java/com/prasoonsoni/supervisedlearninginonlineeducation/SplashActivity.java
@@ -9,30 +9,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class SplashActivity extends AppCompatActivity {
 
-    TextView introText;
+    GifImageView introImage;
     LottieAnimationView introAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        introText = findViewById(R.id.introText);
+        introImage = findViewById(R.id.introImage);
         introAnimation = findViewById(R.id.introAnimation);
 
-        introText.animate().alpha(1f).setDuration(1500);
-        introText.animate().translationYBy(2000f).setDuration(600);
+        introImage.animate().alpha(1f).setDuration(1500);
         introAnimation.animate().alpha(1f).setDuration(1500);
         introAnimation.animate().translationYBy(-2000f).setDuration(600);
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
-            }
-        },3100);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        },2600);
     }
 }
